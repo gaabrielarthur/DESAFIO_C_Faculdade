d354533b70de546983f965797ec5405fa0f1197c
Gabriel.arthur <gabrielarthur01mg@gmail.com>
2025-09-24 10:56:07 -0300

    Add files via upload

---
+++ DESAFIO.pesototal.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void) {
+double pesoCarga1 = 1033.49;
+double pesoCarga2 = 849.88;
+int pesoTotalCargas = pesoCarga1 + pesoCarga2;
+
+printf("Peso total cargas: %d\n", pesoTotalCargas);
+return 0;
+}
