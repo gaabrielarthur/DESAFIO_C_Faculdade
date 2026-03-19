2fab812d1759df65ee378505bb404d7d73025848
Gabriel.arthur <gabrielarthur01mg@gmail.com>
2026-03-19 17:20:16 -0300

    Update LPGD.12.C

--- LISTA1ao12.C/LPGD.12.C
+++ LISTA1ao12.C/LPGD.12.C
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/*
+ Faça uma função recursiva que receba um número inteiro positivo ímpar N e retorne o fatorial
+duplo desse número. O fatorial duplo é definido como o produto de todos os números naturais
+ímpares de 1 até algum número natural ímpar N. Assim, o fatorial duplo de 5 é: 5!! = 1 ∗ 3 ∗ 5
+= 15
+*/
 
 int fatDuplo(int n) {
     if (n == 1)
